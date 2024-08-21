887f81c7e7d578dd98d3e4f58287f01c04806176
Steve <steve.h.rhee@gmail.com>
2024-08-21 16:59:46 -0700

    add non-float iq generation

--- tests/vbGenIoTest.py
+++ tests/vbGenIoTest.py
@@ -6,15 +6,17 @@ def save_test():
     from tGens import noise5
     from vbgensIo import grFile
     # set rf parameters
-    fSamp = 10.0e6
+    fSamp = 61.44e6
     fCent = 1.0e9
     # instantiate noise source
-    noiseObj = noise5(fs=fSamp, bw=6.0e6, tLen=0.001, tPw=1/4.0e5, nBins=128)
+    noiseObj = noise5(fs=fSamp, bw=20.0e6, tLen=0.100, tPw=1/4.0e5, nBins=64)
     # generate sig and plots
     sig = test_gen(noiseObj)
-    test_plot(sig, fs=fSamp, figId=4)
+    sig = sig / np.std(sig)
+    sig = np.round(sig * (2**15 - 1))
+    #test_plot(sig, fs=fSamp, figId=4)
     # generate file
-    fileWrite = grFile('data/noise5', fc=fCent, fs=fSamp, dataType='<f4')
+    fileWrite = grFile('data/noise5', fc=fCent, fs=fSamp, dataType='<i2')
     fileWrite.write_data(sig)
 
 def test_noise():
